03f844bea4627684a70407f72d2a3e0214e85399
James Koduah <jameskoduah171@gmail.com>
2023-03-31 15:05:36 +0000

    betty mistake

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,7 +12,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	int is_leaf = 0;
 
 	if (node == NULL)
-		return 0;
+		return (0);
 
 	if (node->left == NULL)
 		is_leaf += 1;
@@ -21,8 +21,8 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		is_leaf += 1;
 
 	if (is_leaf > 1)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 
 }
