a429a4625e31b3fefac70f734f6e8b7584066d7a
James Koduah <jameskoduah171@gmail.com>
2023-04-27 23:09:15 +0000

    14-binary_tree_balance.c: second

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,24 +6,41 @@
  * Return: height of tree
  */
 
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height = 0;
-	int right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		left_height = binary_tree_balance(tree->left) + 1;
+		left_height = binary_tree_height(tree->left) + 1;
 	else
-		left_height = 0;
+		left_height = 1;
 
 	if (tree->right)
-		right_height = binary_tree_balance(tree->right) + 1;
+		right_height = binary_tree_height(tree->right) + 1;
 	else
-		right_height = 0;
+		right_height = 1;
 
-	return left_height - right_height;
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
 
+/**
+ * binary_tree_balance - check the balance of a tree
+ * @tree: tree node
+ * Return: something
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - 
+				binary_tree_height(tree->right));
+	}
+	return (0);
 }
